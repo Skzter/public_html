ca3e68800f5a7d8237cc8898b7f8424b36102c02
Vincent <147661349+Skzter@users.noreply.github.com>
2024-01-17 20:33:53 +0100

    schriftart geändert + suchleiste bei index.html erstellt

--- javascript_main.js
+++ javascript_main.js
@@ -1,5 +1,7 @@
 //javascript_main
 
-
-let test = "haha hihi";
-//alert(test);
+function suche()
+{
+    var eingabe_suchfeld = document.getElementById('suchleiste').value;
+    alert(eingabe_suchfeld);
+}
